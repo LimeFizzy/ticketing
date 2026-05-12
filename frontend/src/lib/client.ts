0c7c3167a71735706e9d16e550d525a60e5576b5
Augustas Budnikas <augustas@budnikas.dev>
2026-05-13 01:12:48 +0300

    feat(UI): Update Client Config

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -5,5 +5,5 @@ export const createClientConfig: CreateClientConfig = (config) => ({
   baseUrl:
     typeof window === 'undefined'
       ? process.env.INTERNAL_API_BASE_URL!
-      : process.env.NEXT_PUBLIC_BASE_URL!,
+      : undefined,
 });
